942882f3eb944d9fea872f555dc86a07cda0dae5
fabriceo <fabrice.oudert@gmail.com>
2026-02-02 19:05:11 +0100

    change dependencies

--- lib_xua/lib_build_info.cmake
+++ lib_xua/lib_build_info.cmake
@@ -36,10 +36,8 @@ set(LIB_DEPENDENT_MODULES "lib_locks(2.3.1)"
                           "lib_spdif(6.2.1)"
                           "lib_xassert(4.3.1)"
                           "lib_adat(2.0.1)"
-                          "lib_mic_array(5.5.0)"
-                          "lib_sw_pll(2.4.0)"
                           "lib_xud(3.0.1)")
-
+# "lib_mic_array(5.5.0)" "lib_sw_pll(2.4.0)"
 
 set(LIB_COMPILER_FLAGS -O3 -fasm-linenum -fcomment-asm)
 
